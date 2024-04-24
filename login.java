a1aa487c72a6ef4e84ebe19cd2d75c2a1fe6f0e6
SumaGuruvu <161418447+SumaGuruvu@users.noreply.github.com>
2024-04-24 17:04:44 +0530

    Add files via upload

--- login.java
+++ login.java
@@ -1,40 +1,46 @@
 package com.payments;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Dashboardservlet
- */
-@WebServlet("/Dashboardservlet")
-public class Dashboardservlet extends HttpServlet {
+
+@WebServlet("/login")
+public class login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Dashboardservlet() {
+    
+    public login() {
         super();
-        // TODO Auto-generated constructor stub
+       
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String phonenumber=request.getParameter("phno");
+		String password=request.getParameter("password");
+		paymentsappDAO pd=new paymentsappDAO();
+		if(pd.validlogin(phonenumber, password))
+		{
+			RequestDispatcher rd=request.getRequestDispatcher("/Dashboard.html");
+			rd.forward(request, response);
+		}
+		else
+		{
+			response.getWriter().write("invalid");
+			RequestDispatcher rd=request.getRequestDispatcher("/Dashboard.html");
+			rd.include(request, response);
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 	}
 
