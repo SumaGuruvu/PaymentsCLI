3553e3ed39027eb99c72dde7df0a3a7b5e05ce2d
SumaGuruvu <161418447+SumaGuruvu@users.noreply.github.com>
2024-05-07 13:45:52 +0530

    Add files via upload

---
+++ users.java
@@ -0,0 +1,67 @@
+package com.payments;
+
+import java.util.Date;
+
+public class users {
+	
+	String FirstName;
+	String LastName;
+	String PhoneNo;
+	Date DateOfBirth;
+	String Email;
+	String Password;
+	String Address;
+	double Currbal;
+	
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public String getPhoneNo() {
+		return PhoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		PhoneNo = phoneNo;
+	}
+	public Date getDateOfBirth() {
+		return DateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		DateOfBirth = dateOfBirth;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public double getCurrbal() {
+		return Currbal;
+	}
+	public void setCurrbal(double currbal) {
+		Currbal = currbal;
+	}
+	
+	
+
+}
